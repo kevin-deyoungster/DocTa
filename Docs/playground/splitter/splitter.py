d9f73152638e5307d6c4c2bb0f002864ebb37a89
Kevin de Youngster <kevin.deyoungster@gmail.com>
2018-08-09 12:41:43 +0000

    Splitter algorithm is ready

--- Docs/playground/splitter/splitter.py
+++ Docs/playground/splitter/splitter.py
@@ -3,19 +3,28 @@ from werkzeug import secure_filename
 import os
 import shutil
 
+
+def wrap_in(string, wrap_tag):
+    '''
+    Wraps [string] in html tag [wrap_tag]
+    '''
+    open = f"<{wrap_tag}>"
+    close = f"</{wrap_tag}>"
+    return BeautifulSoup(f"{open}{string}{close}", "html.parser")
+
+
 PREPEND_FOLDER_NAME = "-prepend"
 
 
 def get_sections(soup, separator_symbol):
     '''
-    Takes in a beautiful souped html and split its according to 'chosen separator'
+    Takes in a beautiful souped html and splits its according to 'chosen separator'
     Assumptions: The converted file must have a <body> tag
     '''
     body = soup.find('body')
     body_text = body.text.strip()
 
-    if separator_symbol in body_text:  # There is a separator, go ahead
-
+    if separator_symbol in body_text:  # There is a separator, go ahea
         sections = {}
         temp_html = ""
         current_heading = ""
@@ -46,85 +55,77 @@ def get_sections(soup, separator_symbol):
     else:  # there is no separator, don't bother yourself
         return soup
 
-# Takes a list of tags and converts it to html string ( soup )
 
-
-def wrap_in(string, wrap_tag):
-    open = f"<{wrap_tag}>"
-    close = f"</{wrap_tag}>"
-    return BeautifulSoup(f"{open}{string}{close}", "html.parser")
-
-
-def soupify_list(tag_list):
-    # Takes a list of bs4 tags and returns a soup of them
-    soup = BeautifulSoup("", "html.parser")
-    body = soup.new_tag("body")
-    for tag in tag_list:
-        body.append(tag)
-    soup.append(body)
-    return soup
-
-
-def sectionize(html, separators):
+def split(html, separators):
+    '''
+    The main splitting code. Recursive algorithm
+    '''
     if separators:
-        current_sep = separators.pop(0)
+        first_separator = separators.pop(0)
         html_text = html.get_text()
-        if current_sep not in html_text:
+        if first_separator not in html_text:
             return html
         else:
-            soup = get_sections(html, current_sep)
-            for level in soup.keys():
-                soup[level] = sectionize(soup[level], separators.copy())
+            soup = get_sections(html, first_separator)
+            for section in soup.keys():
+                soup[section] = split(soup[section], separators.copy())
             return soup
     else:
         return html
 
 
 def saveSoupToHTML(soup, file_path):
+    '''
+    Saves beautiful soup object to html file
+    '''
     try:
         with open(file_path, "wb") as f:
             f.write(soup.prettify("utf-8"))
             return True
     except Exception as e:
-        print(f"Could not save soup. Errro: {e}")
+        print(f"Could not save soup. Error: {e}")
         return False
 
 
-def createFolderTree(sectionized, parent_dir):
+def createFolderTree(sections, parent_dir):
     '''
-    Takes in a sectionized document and create corresponding folders. Recursive
+    Takes in sections and creates corresponding folders. Recursive
     '''
-    if type(sectionized) == dict:
-        for level in sectionized:
-            print(f"level::: {level}")
-            level_dir = os.path.join(parent_dir, secure_filename(level))
-            print(f"Level_Dir::: {level_dir}")
-            if not os._exists(level_dir):
-                os.makedirs(level_dir)
-            createFolderTree(sectionized[level], level_dir)
-    else:
-        # It must be just a string i.e. BASE CASE so create the HTML file
+    if type(sections) == dict:  # Section has subsections
+        for heading in sections:
+            print(f"Heading::: {heading}")
+            heading_dir = os.path.join(parent_dir, secure_filename(heading))
+            print(f"Heading Dir::: {heading_dir}")
+            if not os._exists(heading_dir):
+                os.makedirs(heading_dir)
+            createFolderTree(sections[heading], heading_dir)
+    elif type(sections) == BeautifulSoup:  # Section is actually html code. Base case
         html_path = os.path.join(parent_dir, "index.html")
-        print(f"HTML_File::: {html_path}\n")
-        saveSoupToHTML(sectionized, html_path)
+        print(f"::HTML_File::: {html_path}\n")
+        saveSoupToHTML(sections, html_path)
 
-        # Export the images to the files
-        for img in sectionized.find_all('img'):
+        # Export the images in this file to its directory
+        for img in sections.find_all('img'):
             image_path = img.get('src')
             if image_path:
                 shutil.move(image_path, os.path.join(parent_dir, image_path))
+    else:
+        print(f"<===> Eih, What is a {type(sections)}")
 
 
 def test(html_path):
     with open(html_path, encoding="utf-8") as f:
         soup = BeautifulSoup(f.read(), "html.parser")
-        ssoup = sectionize(soup, ['~', '@'])
-        if not os.path.exists("tester"):
-            os.mkdir("tester")
+        sections = split(soup, ['~', '@'])
+
+        FOLDER = "tester"
+        if not os.path.exists(FOLDER):
+            os.mkdir(FOLDER)
         else:
-            shutil.rmtree("tester")
-            os.mkdir("tester")
-        createFolderTree(ssoup, "tester")
+            shutil.rmtree(FOLDER)
+            os.mkdir(FOLDER)
+
+        createFolderTree(sections, FOLDER)
 
 
 test("test.html")
