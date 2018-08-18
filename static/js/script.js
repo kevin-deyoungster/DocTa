57305fe186a603b7ed5aa2db39f62204aec5e163
Kevin de Youngster <kevin.deyoungster@gmail.com>
2018-08-18 17:09:10 +0000

    added font support

--- static/js/script.js
+++ static/js/script.js
@@ -1,88 +1,88 @@
 let ALL_FILES = [];
-const FILE_LIST = document.getElementById('dropped-files');
-const COUNT_VIEW = document.getElementById('count');
-const FORM = document.getElementById('form');
-const FILE_INPUT = document.getElementById('file');
-const SUBMIT_BUTTON = document.getElementById('submitButton');
-const CLEAR_LIST_BUTTON = document.getElementById('clearList');
+const FILE_LIST = document.getElementById("dropped-files");
+const COUNT_VIEW = document.getElementById("count");
+const FORM = document.getElementById("form");
+const FILE_INPUT = document.getElementById("file");
+const SUBMIT_BUTTON = document.getElementById("submitButton");
+const CLEAR_LIST_BUTTON = document.getElementById("clearList");
 
 function renderDocs() {
-	FILE_LIST.innerHTML = '';
-	COUNT_VIEW.innerText = ALL_FILES.length + ' Documents';
+  FILE_LIST.innerHTML = "";
+  COUNT_VIEW.innerText = ALL_FILES.length + " Documents";
 
-	ALL_FILES.forEach(file => {
-		let li = document.createElement('li');
-		li.className = 'dropped-file';
-		li.innerText = file.name;
-		let span = document.createElement('span');
-		span.innerText = 'X';
-		span.className = 'dropped-file-close';
-		span.onclick = () => {
-			ALL_FILES.splice(ALL_FILES.indexOf(file), 1);
-			resetInput();
-			renderDocs();
-		};
-		li.appendChild(span);
-		FILE_LIST.appendChild(li);
-	});
+  ALL_FILES.forEach(file => {
+    let li = document.createElement("li");
+    li.className = "dropped-file";
+    li.innerText = file.name;
+    let span = document.createElement("span");
+    span.innerText = "X";
+    span.className = "dropped-file-close";
+    span.onclick = () => {
+      ALL_FILES.splice(ALL_FILES.indexOf(file), 1);
+      resetInput();
+      renderDocs();
+    };
+    li.appendChild(span);
+    FILE_LIST.appendChild(li);
+  });
 }
 
 FILE_INPUT.addEventListener(
-	'change',
-	function() {
-		ALL_FILES = Array.from(this.files).concat(Array.from(ALL_FILES));
-		renderDocs();
-	},
-	false
+  "change",
+  function() {
+    ALL_FILES = Array.from(this.files).concat(Array.from(ALL_FILES));
+    renderDocs();
+  },
+  false
 );
 
 CLEAR_LIST_BUTTON.onclick = () => {
-	clearFiles();
-	resetInput();
-	renderDocs();
+  clearFiles();
+  resetInput();
+  renderDocs();
 };
 
 FORM.onsubmit = function(event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	let formData = new FormData();
-	for (let file of ALL_FILES) {
-		formData.append('file[]', file);
-	}
+  let formData = new FormData();
+  for (let file of ALL_FILES) {
+    formData.append("file[]", file);
+  }
 
-	SUBMIT_BUTTON.value = 'Converting...';
-	axios({
-		method: 'post',
-		url: '/convert',
-		data: formData,
-		responseType: 'arraybuffer',
-		config: { headers: { 'Content-Type': 'multipart/form-data' } }
-	})
-		.then(response => {
-			filename = response.headers['content-disposition'].split('=')[1];
-			downloadFile(response.data, filename);
-			SUBMIT_BUTTON.value = 'Convert';
-		})
-		.catch(err => {
-			SUBMIT_BUTTON.value = 'Convert';
-			alert(err);
-		});
-	return false;
+  SUBMIT_BUTTON.value = "Converting...";
+  axios({
+    method: "post",
+    url: "/convert",
+    data: formData,
+    responseType: "arraybuffer",
+    config: { headers: { "Content-Type": "multipart/form-data" } }
+  })
+    .then(response => {
+      filename = response.headers["content-disposition"].split("=")[1];
+      downloadFile(response.data, filename);
+      SUBMIT_BUTTON.value = "Convert";
+    })
+    .catch(err => {
+      SUBMIT_BUTTON.value = "Convert";
+      alert(err);
+    });
+  return false;
 };
 
 function downloadFile(data, filename) {
-	const url = window.URL.createObjectURL(new Blob([data]));
-	const link = document.createElement('a');
-	link.href = url;
-	link.setAttribute('download', filename);
-	document.body.appendChild(link);
-	link.click();
+  const url = window.URL.createObjectURL(new Blob([data]));
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", filename);
+  document.body.appendChild(link);
+  link.click();
 }
 
 function clearFiles() {
-	ALL_FILES = [];
+  ALL_FILES = [];
 }
 
 function resetInput() {
-	FILE_INPUT.value = '';
+  FILE_INPUT.value = "";
 }
